dc6eb29037725c0c77ff165a3c5be8e3426e707a
largefullmoon <largefullmoon@gmail.com>
2024-12-23 23:59:43 +0200

    remove mini message bar on message page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,29 +1,36 @@
-import React, {useRef} from 'react';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { Outlet, useLocation } from 'react-router-dom';
 import UserListModal from './components/UserListModal';
 import CollapsibleWindow from './components/CollapsibleWindow';
 
 const Layout = () => {
     const windows = useSelector(state => state.windows.windows);
+    const location = useLocation();
+    const isMessagePage = location.pathname === '/message';
+
     return (
         <div>
             <Outlet />
-            {/* User List Modal */}
-            <UserListModal />
-            {/* Chat Windows */}
-            <div>
-                {windows.map((window, index) => (
-                    <CollapsibleWindow
-                        key={`${window.senderId}-${window.receiverId}`}
-                        senderId={window.senderId}
-                        receiverId={window.receiverId}
-                        position={index}
-                        receivedUser={window.receivedUser}
-                        isCollapsed={window.isCollapsed}
-                    />
-                ))}
-            </div>
+            {!isMessagePage && (
+                <>
+                    {/* User List Modal */}
+                    <UserListModal />
+                    {/* Chat Windows */}
+                    <div>
+                        {windows.map((window, index) => (
+                            <CollapsibleWindow
+                                key={`${window.senderId}-${window.receiverId}`}
+                                senderId={window.senderId}
+                                receiverId={window.receiverId}
+                                position={index}
+                                receivedUser={window.receivedUser}
+                                isCollapsed={window.isCollapsed}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
